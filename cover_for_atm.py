b0a6b91df05f94252150911e541edab0a8812c48
AndrasWanderer <135740822+AndrasWanderer@users.noreply.github.com>
2024-07-29 20:47:18 +0300

    Refactoring begins

--- cover_for_atm.py
+++ cover_for_atm.py
@@ -45,8 +45,12 @@ def check_sum(operation):
     try:
         start_sum += int(value)
         start_sum = int(wealth_tax(start_sum))
+        # if wealth_tax == True:
+        #         atm_info.insert(0, "Вычтен процент налога на богатство")
         count_replainishment += 1
         start_sum = percent_rep(start_sum,count_replainishment)
+        # if percent_rep == True:
+        #         atm_info.insert(0, "Начислен процент за третье пополнение")
         print(start_sum)
     except:
         traceback.print_exc()
@@ -63,8 +67,7 @@ def del_sum(operation):
     if a != None:
         atm_entry.delete(0, tk.END)
         atm_entry.insert(0, a)
-
-
+        
     elif start_sum < int(atm_entry.get()):
         atm_balance.delete(0, tk.END)
         atm_info.insert(0, f"Ошибка! Недостаточно средств!")
@@ -73,9 +76,15 @@ def del_sum(operation):
         try:
             start_sum -= int(value)
             start_sum = withdrawal_percent(start_sum,amount)
+            # if withdrawal_percent == True:
+            #     atm_info.insert(0, "Вычтен процент за снятие")
             start_sum = wealth_tax(start_sum)
+            # if wealth_tax == True:
+            #     atm_info.insert(0, "Вычтен процент налога на богатство")
             count_replainishment += 1
             start_sum = percent_rep(start_sum,count_replainishment)
+            # if percent_rep == True:
+            #     atm_info.insert(0, "Начислен процент за третье пополнение")
             print(start_sum)
         except:
             traceback.print_exc()
